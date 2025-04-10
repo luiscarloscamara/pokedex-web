00bd59a54dd51d4b6352dd4573596d2156c2abed
Luís Carlos <lcscamara.cic@uesc.br>
2025-04-10 12:29:04 -0300

    :sparkles: feat: estilizar lista de pokemons

--- src/components/ListPokemons.vue
+++ src/components/ListPokemons.vue
@@ -5,7 +5,7 @@ const pokemon = defineProps(["name", "urlImgPoke"])
 
 <template>
     <div class="col-4">
-        <div class="card p-2 mb-3">
+        <div class="card p-2 mb-3 cardListPokemon">
             <p class="text-center">{{ pokemon.name }}</p>
             <img 
             :src="urlImgPoke" class="card-img-top" 
@@ -16,6 +16,17 @@ const pokemon = defineProps(["name", "urlImgPoke"])
     </div>
 </template>
 
-<style>
-
+<style scoped>
+    .cardListPokemon{
+        background: rgb(72,63,251);
+        background: radial-gradient(circle, rgba(94, 87, 232, 0.6) 0%, rgba(70,252,244,0.2) 100%);
+        cursor: pointer;
+    }
+    .cardListPokemon:hover{
+        background: rgb(72,63,251);
+        background: radial-gradient(circle, rgba(72,63,251,0.6) 0%, rgba(70,252,244,0.4) 100%);
+    }
+    .cardListPokemon img:hover{
+        padding: .1rem;
+    }
 </style>
