df232e0356e3e822f219e272bec4f710eb1f4bca
Luís Carlos <lcscamara.cic@uesc.br>
2025-04-10 10:59:02 -0300

    :sparkles: feat: criar componente para lista de pokemons e oraganizar em card

---
+++ src/components/ListPokemons.vue
@@ -0,0 +1,16 @@
+<script setup>
+const pokemon = defineProps(["name"])
+
+</script>
+
+<template>
+    <div class="col-4">
+        <div class="card mb-3">
+            <p class="text-center">{{ pokemon.name }}</p>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
